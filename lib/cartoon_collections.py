22215002effc485daf9135ccf89705c1113ab4a1
Maryannewambui <trimbleleewambui@gmail.com>
2023-11-28 06:25:34 +0300

    assignment done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,16 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_names):
+    for index, name in enumerate(dwarf_names):
+        print(f"{index + 1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(veggies):
+    return [veggie.capitalize() + '!' for veggie in veggies]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+        return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheese_types:
+            return snack
+    return None
